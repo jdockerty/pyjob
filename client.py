f466cf173585d6cfe047fd0eb6e730953034429b
Jack Dockerty <jdockerty19@gmail.com>
2021-02-21 22:19:19 +0000

    Pushing URL out to build function, adding set location.

--- client.py
+++ client.py
@@ -37,19 +37,33 @@ class Search(object):
 
     def set_keyterms(self, keyterms: list):
         
-        assert len(keyterms) != 0
-        
-        if len(keyterms) == 1:
-            self.search_keyterms = keyterms[0]
-        else:
-            self._search_keyterms = []
-            for keyword in keyterms:
-                self._search_keyterms.append(keyword)
-    
+        try:
+            assert len(keyterms) != 0
+            
+            if len(keyterms) == 1:
+                self.search_keyterms = keyterms[0]
+            else:
+                self._search_keyterms = []
+                for keyword in keyterms:
+                    self._search_keyterms.append(keyword)
+
+        except AssertionError:
+            logger.info("Key terms must be populated")
+            raise
+            
     def set_location(self, location: str):
         
         self._location = location
+    
+    def set_location_distance(self, distance: int):
         
+        try:
+            assert distance >= 0
+            self._distance_from_location = distance
+            logger.debug("Distance from location set to {}", self._distance_from_location)
+            
+        except AssertionError as err:
+            logger.info("Cannot have a distance lower than 0, using default of 10.")
     
     def _build_url(self):
         
@@ -63,8 +77,7 @@ class Search(object):
             # for keyword in self._search_keyterms:
             #     url += f"%20{keyword}"
             url += f"&keywords={self._search_keyterms}" # Check whether list items affect the query params in URL.
-
-            
+  
         if self._location is None:
             logger.debug("No location set.")
         else:
@@ -92,5 +105,6 @@ class Search(object):
 s = Search()
 s.set_keyterms(["devops engineer", "software engineer"])
 s.set_location("London")
+s.set_location_distance(50)
 # s.search()
 s._build_url()
