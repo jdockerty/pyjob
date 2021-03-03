55fb62cf51854f82aba25d888c5be85533aaf14e
Jack Dockerty <jdockerty19@gmail.com>
2021-03-03 16:15:07 +0000

    Updating author

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     
 setup(
     name="pyjob-jdockerty",
-    author="Jack Dockerty",
+    author="jdockerty",
     description="Job searching through code, interaction to the Reed API.",
     long_description=long_description,
     long_description_content_type="text/markdown",
